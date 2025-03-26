7743896dad1d4056081797d2e8ed96be69445a0d
phoepner <patrick-hoepner@outlook.de>
2025-03-26 22:02:39 +0100

    animation for inital load, has to be adjustet for login window

---
+++ scripts/animationTest.js
@@ -0,0 +1,12 @@
+
+
+
+function animateLogo(){
+    const logoRef = document.getElementById("logo");
+    logoRef.classList.toggle("animate");
+}
+
+function fadeAllElementsIn(){
+    const btn = document.getElementById("animateBtn")
+    btn.classList.toggle("fadeIn");
+}
