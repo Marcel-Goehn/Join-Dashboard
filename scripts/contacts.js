a3612859b58aa16927074881ef334493c3a8fc05
phoepner <patrick-hoepner@outlook.de>
2025-03-30 14:40:54 +0200

    contact logic works now

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,45 +1,46 @@
-const databaseLinkRef = "https://join---database-default-rtdb.europe-west1.firebasedatabase.app/users.json";
+const databaseLinkRef = "https://join---database-default-rtdb.europe-west1.firebasedatabase.app/users/";
 const contactDiv = document.getElementById("contactNameDiv");
 
-async function fetchData(){
-    const response = await fetch(databaseLinkRef);
-    const data = await response.json();
-    console.log(data);
-    return data;
+async function fetchContactData(user){
+    const response = await fetch(`https://join---database-default-rtdb.europe-west1.firebasedatabase.app/users/${user}/contacts.json`);
+    const contactData = await response.json();
+    console.log(contactData);
+    return contactData;
 }
+async function sortContacts(contactData){
+    const sortedContacts = Object.entries(contactData).sort(([a], [b]) => a.localeCompare(b));
+    console.log(sortedContacts);
+    const sortedContactsObject = Object.fromEntries(sortedContacts);
+    console.log(sortedContactsObject);
+    return sortedContactsObject;
+ }
 
 
-
-
-async function sortContacts(user){
-    const users = await fetchData();
-    Object.entries(users[user].contacts).sort((a,b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
-    return users;
-}
-
-
-function renderContacts(users, user){
+function renderContacts(contactData){
     let previousLetter = "";
-    Object.keys(users[user].contacts).forEach((contact)=>{
+    for(const contact in contactData){
         if(contact.charAt(0).localeCompare(previousLetter) > 0){
-            contactDiv.innerHTML += letterTemp(contact.charAt(0).toUpperCase);
+            contactLetter = contact.charAt(0).toUpperCase();
+            console.log(contactLetter)
+            contactDiv.innerHTML += letterTemp(contactLetter);
             previousLetter = contact.charAt(0);
         }
-        contactDiv.innerHTML += contactTemp(users[user][contact].email, contact, users[user][contact].phone);
-
-    })
-    
+        console.log(contactData[contact].email, contact, contactData[contact].phone);
+        contactDiv.innerHTML += contactTemp(contactData[contact].email, contact, contactData[contact].phone);
+    }
 }
+
 
 function shorthandName(name){
     return name.split(" ").map(partName => partName[0].toUpperCase()).join("");
 }
 
-function openContact(email, name, phone){
-
+async function showContacts(user){
+    const contactData = await fetchContactData(user);
+    const sortedContacts = await sortContacts(contactData); //
+    renderContacts(sortedContacts);
 }
 
-async function showContacts(user){
-    const sortedUsers = await sortContacts(user);
-    renderContacts(sortedUsers, user);
+function openContact(email, name, phone){
+
 }
