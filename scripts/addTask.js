8f2f42baf69bbffc83744b42b1783de28ff705b9
Aidan-Vidal <alaindavid.roth@web.de>
2025-04-04 19:12:55 +0200

    Tasks are now stored in Firebase

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -1,7 +1,8 @@
-const databaseLinkRef = "https://join---database-default-rtdb.europe-west1.firebasedatabase.app/users/";
+const databaseLinkRef = "https://join---database-default-rtdb.europe-west1.firebasedatabase.app/";
 let logged_user = {};
 let priority = "medium";
-let highlighted_contacts = [];
+let assigned = {};
+let subtasks = {};
 
 function select(num) {
     resetButtons();
@@ -48,34 +49,9 @@ function resetButtons() {
 async function fetchUser() {
     let result = sessionStorage.getItem("loggedIn");
     let userID = await JSON.parse(result);
-    const response = await fetch(databaseLinkRef + userID + ".json");
+    const response = await fetch(databaseLinkRef + "users/" + userID + ".json");
     logged_user = await response.json();
     tempContacts();
-}
-
-function tempContacts(array = Object.values(logged_user.contacts)) {
-    document.getElementById('contactsList').innerHTML = "";
-    for (let index = 0; index < array.length; index++) {
-        const contact = array[index];
-        console.log(contact);
-        
-        document.getElementById('contactsList').innerHTML +=
-        `<div class="space-between contact" id="${contact.name}" onclick="highlightContact('${contact.name}')">
-            <div class="indice_name">
-                <div class="circle centered">${indices(contact.name)}</div>
-                <span>${contact.name}</span>
-            </div>
-            <img id="${contact.name}-unchecked" src="../assets/img/Check button_unchecked.svg" alt="">
-            <svg id="${contact.name}-checked" class="hidden" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#2a3547">
-                <path d="M20 11V17C20 18.6569 18.6569 20 17 20H7C5.34315 20 4 18.6569 4 17V7C4 5.34315 5.34315 4 7 4H15" stroke="white" stroke-width="2" stroke-linecap="round"/>
-                <path d="M8 12L12 16L20 4.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-        </div>`
-    }
-    highlighted_contacts.forEach(element => {
-        document.getElementById(element).classList.toggle('highlight');
-        document.getElementById(`${element}-unchecked`).classList.toggle('hidden');
-        document.getElementById(`${element}-checked`).classList.toggle('hidden');})
 }
 
 function toggleContacts() {
@@ -91,9 +67,8 @@ function rotateIcon(id) {
 
 function closedContacts() {
     document.getElementById('closed_contacts').innerHTML = "";
-    for (let index = 0; index < highlighted_contacts.length; index++) {
-        const element = highlighted_contacts[index];
-        document.getElementById('closed_contacts').innerHTML += `<div class="circle centered">${indices(element)}</div>`;
+    for (const name of Object.values(assigned)) {
+        document.getElementById('closed_contacts').innerHTML += `<div class="circle centered">${indices(name)}</div>`;
     }
 }
 
@@ -103,18 +78,18 @@ function indices(name) {
     return step_2.join("");
 }
 
-function highlightContact(contact) {
-    document.getElementById(contact).classList.toggle('highlight');
-    document.getElementById(`${contact}-unchecked`).classList.toggle('hidden');
-    document.getElementById(`${contact}-checked`).classList.toggle('hidden');
-    currentlyHighlighted(contact);
+function highlightContact(contact, id) {
+    document.getElementById(id).classList.toggle('highlight');
+    document.getElementById(`${id}-unchecked`).classList.toggle('hidden');
+    document.getElementById(`${id}-checked`).classList.toggle('hidden');
+    editAssignedObject(contact, id);
 }
 
-function currentlyHighlighted(contact) {
-    if (document.getElementById(`${contact}-checked`).classList != 'hidden') {
-        highlighted_contacts.includes(contact) ? null : highlighted_contacts.push(contact);
+function editAssignedObject(contact, id) {
+    if (document.getElementById(`${id}-checked`).classList != 'hidden') {
+        assigned[`${id}`] ? null : assigned[`${id}`] = contact;
     } else {
-        highlighted_contacts.splice(highlighted_contacts.indexOf(contact), 1)
+        delete assigned[`${id}`];
     }
 }
 
@@ -123,8 +98,7 @@ function search() {
     document.getElementById('closed_contacts').classList.add('hidden');
     let search = document.getElementById('searchbar').value.toLowerCase();
     let result = Object.values(logged_user.contacts).filter(details => details.name.toLowerCase().includes(search));
-    console.log(result);
-    tempContacts(Object.values(result));
+    tempContacts(Object.entries(result));
 }
 
 function toggleHidden(id) {
@@ -149,59 +123,35 @@ function clearInput() {
     iconsDuringInput();
 }
 
-
-function iconsDuringInput() {
-    if (document.getElementById('subtask_input').value !== "") {
-        document.getElementById('subtask_input_icons').innerHTML = `
-        <img class="hover" onclick="clearInput()" src="../assets/img/addtask/x.svg"></img>
-        |
-        <img class="hover" onclick="addSubtask()" src="../assets/img/addtask/done_black.svg"></img>`
-    } else {
-        console.log("blub");
-        document.getElementById('subtask_input_icons').innerHTML = `
-        <img class="hover" src="../assets/img/addtask/add.svg"></img>`
-    }
-}
-
 function addSubtask() {
     let subtask = document.getElementById('subtask_input').value;
     document.getElementById('subtask_input').value = "";
     iconsDuringInput();
-    let id = Math.random();
-        document.getElementById('addedSubtasks').innerHTML += `
-            <li class="space-between addedSubtask" id="${id}">
-                <span id="subtask_row${id}" class="listitem">${subtask}</span>
-                <div class="addSubtask_btndiv">
-                    <img class="hover" onclick="editSubtask(${id})" src="../assets/img/addtask/edit.svg">
-                    |
-                    <img class="hover" onclick="deleteSubtask(${id})" src="../assets/img/addtask/delete.svg">
-                </div>
-            </li>
-            <div id="edit_div${id}" class="space-between hidden edittedSubtask">
-            <input id="edit_input${id}" type="text">
-                <div class="addSubtask_btndiv">
-                    <img class="hover" onclick="deleteSubtask(${id})" src="../assets/img/addtask/delete.svg">
-                    |
-                    <img class="hover" onclick="confirmSubtask(${id})" src="../assets/img/addtask/done_black.svg">
-                </div>
-            </div>`;
+    let id = Math.random().toString().replace(".","");
+    subtask_template(id, subtask);
+    subtasks[`${id}`] = subtask;
 }
+
 function confirmSubtask(id) {
     document.getElementById(`edit_div${id}`).classList.toggle('hidden');
-    document.getElementById(`edit_div${id}`).style = "background-color: white"
+    document.getElementById(`edit_div${id}`).style = "background-color: white";
     document.getElementById(id).classList.toggle('hidden');
     document.getElementById(`subtask_row${id}`).innerHTML = document.getElementById(`edit_input${id}`).value;
+    subtasks[`${id}`] = document.getElementById(`edit_input${id}`).value;
 }
+
 function deleteSubtask(id) {
     document.getElementById(id).remove();
     document.getElementById(`edit_div${id}`).remove();
+    delete subtasks[`${id}`];
 }
 
 function editSubtask(id) {
+    let current_subtask = document.getElementById(`subtask_row${id}`).innerHTML;
     document.getElementById(`edit_div${id}`).classList.toggle('hidden');
-    document.getElementById(`edit_div${id}`).style = "background-color: white"
+    document.getElementById(`edit_div${id}`).style = "background-color: white";
     document.getElementById(id).classList.toggle('hidden');
-    document.getElementById(`edit_input${id}`).value = document.getElementById(`subtask_row${id}`).innerHTML;
+    document.getElementById(`edit_input${id}`).value = current_subtask;
 }
 
 function validate() {
@@ -210,39 +160,49 @@ function validate() {
     let category = document.getElementById('btn_text').innerHTML;
     
     if (title !== "" && duedate !== "" && category !== "Select task category") {
-        let task = {
-            "assigned" : highlighted_contacts,
-            "category" : category,
-            "description" : document.getElementById('description').value,
-            "duedate" : duedate,
-            "priority" : priority,
-            "subtasks" : null
-        }
-        uploadTask(task);      
+        uploadTask(assembleTask(duedate, category, title));
     } else {
         console.log("something is missing");
     }
 }
 
-function assembleTask() {
-    let task = {
-        "assigned" : highlighted_contacts,
-        "category" : category,
+function assembleTask(date, cat, header) {
+    return {
+        "assigned" : assigned,
+        "category" : cat,
         "description" : document.getElementById('description').value,
-        "duedate" : duedate,
+        "duedate" : date,
+        "currentStatus" : "To Do",
         "priority" : priority,
-        "subtasks" : null
+        "subtasks" : subtasks,
+        "title" : header
     }
-    uploadTask(task);
 }
 
-async function uploadTask(task) {
-    let result = sessionStorage.getItem("loggedIn");
-    let userID = await JSON.parse(result);
-    fetch(databaseLinkRef + userID + ".json", {
+async function uploadTask(object) {
+    console.log(object);
+    fetch(databaseLinkRef + "test.json", {
         method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(task),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(object)
     });
-    console.log(databaseLinkRef + userID + ".json");
+}
+
+let TestObject = {
+    "Feldversuch" : 5,
+    "assigned" : null,
+    "category" : "User Story",
+    "description" : "this is a testObject",
+    "duedate" : "24-12-2025",
+    "currentStatus" : "To Do",
+    "priority" : "low",
+    "subtasks" : null,
+    "title" : "This is a title"
+}
+
+function test() {
+    let ran_num = Math.random().toString().replace(".","");
+    console.log(ran_num);
+    // TestObject[`${ran_num}`] = "Test";
+    // uploadTask(TestObject);
 }
