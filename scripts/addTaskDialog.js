91a24c6b9d5d763150a387637bb03d950b997a50
phoepn96 <patrick-hoepner@outlook.de>
2025-04-06 22:22:08 +0200

    added slideIn And Out animation

--- scripts/addTaskDialog.js
+++ scripts/addTaskDialog.js
@@ -2,4 +2,14 @@ const addTaskDial = document.getElementById("addTaskDial");
 
 function openAddTask() {
 	addTaskDial.showModal();
+	addTaskDial.classList.remove("slideTaskDialOut");
+	addTaskDial.classList.add("slideTaskDialIn");
+}
+
+function closeAddTask() {
+	addTaskDial.classList.remove("slideTaskDialIn");
+	addTaskDial.classList.add("slideTaskDialOut");
+	setTimeout(() => {
+		addTaskDial.classList.close();
+	}, 1000);
 }
