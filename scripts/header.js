88c3e239dd5a740ce41d8a5e4ceec91181e7fd99
phoepn96 <patrick-hoepner@outlook.de>
2025-04-07 10:52:44 +0200

    header link hover

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,11 @@
-let helpIcon = document.getElementById('headerInfoIcon');
-let userButton = document.getElementById('shorthandNameDiv');
-
+let helpIcon = document.getElementById("headerInfoIcon");
+let userButton = document.getElementById("shorthandNameDiv");
 
 function toggleMenu() {
-    document.getElementById('menu').classList.toggle('hidden');
+	document.getElementById("menu").classList.toggle("hidden");
+	document.getElementById(`menu`).classList.toggle("topToBottom");
 }
 
 function logout() {
-    //loggedIn Pair löschen
+	//loggedIn Pair löschen
 }
