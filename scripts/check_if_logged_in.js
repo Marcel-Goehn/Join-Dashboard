7ed27515854504e43bd7d2fb5ef43a330819d4ff
phoepner <patrick-hoepner@outlook.de>
2025-05-10 01:43:40 +0200

    +

--- scripts/check_if_logged_in.js
+++ scripts/check_if_logged_in.js
@@ -3,7 +3,6 @@ checkIfLoggedIn();
 
 function checkIfLoggedIn(){
 	const userObjectForCheck = sessionStorage.getItem("loggedIn");
-		console.log("test;")
 	if(userObjectForCheck === null || userObjectForCheck === undefined){
 		window.location.href = "../index.html";
 		console.log("NOT CHECKING")
