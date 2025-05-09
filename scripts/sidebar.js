680e79e363bb59d1bb792b5ba3d3b37f0c7fb636
Aidan-Vidal <alaindavid.roth@web.de>
2025-05-09 21:28:25 +0200

    2nd feedback

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -8,32 +8,43 @@ checkIfLoggedIn();
 function highlightSidebarLink() {
 	const currentHtmlSite = window.location.pathname;
 	switch (currentHtmlSite) {
-		case "/html/contacts.html":
+		case "/Join/html/contacts.html":
 			contactsDiv.classList.add("highlighted");
 			break;
 
-		case "/html/summary.html":
+		case "/Join/html/summary.html":
 			summaryDiv.classList.add("highlighted");
 			break;
 
-		case "/html/board.html":
+		case "/Join/html/board.html":
 			boardDiv.classList.add("highlighted");
 			break;
 
-		case "/html/addTask.html":
+		case "/Join/html/addTask.html":
 			addTaskDiv.classList.add("highlighted");
 			break;
 
 		default:
-			addTaskDiv.classList.remove("hightlighted");
-			boardDiv.classList.remove("hightlighted");
-			summaryDiv.classList.remove("hightlighted");
-			contactsDiv.classList.remove("hightlighted");
+			addTaskDiv.classList.remove("highlighted");
+			boardDiv.classList.remove("highlighted");
+			summaryDiv.classList.remove("highlighted");
+			contactsDiv.classList.remove("highlighted");
 	}
 }
 
 
 function checkIfLoggedIn(){
+	const path = window.location.pathname;
+	if (path.includes('legalNotice.html') || path.includes('privacyPolicy.html')) {
+			if (sessionStorage.getItem("loggedIn") == null) {
+		document.getElementById('loginSidebarDiv').classList.remove('hidden');
+		document.getElementById('navLinksDiv').classList.add('hidden');
+			} else {
+		document.getElementById('loginSidebarDiv').classList.add('hidden');
+		document.getElementById('navLinksDiv').classList.remove('hidden');
+			}
+		return;
+	}
 	if(userObjectForCheck === null || undefined){
 		window.location.href = "../index.html";
 	}
