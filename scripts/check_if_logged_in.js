eedb99fdca1a7db971891cecdcb1e63a9ecbc222
Marcel Göhn <marcelgoehn@googlemail.com>
2025-05-10 13:04:11 +0200

    fixed const userObjectForCheck = sessionStorage.getItem('loggedIn'); has already been declared bug

--- scripts/check_if_logged_in.js
+++ scripts/check_if_logged_in.js
@@ -1,4 +1,3 @@
-const userObjectForCheck = sessionStorage.getItem("loggedIn");
 checkIfLoggedIn();
 
 
@@ -6,6 +5,7 @@ checkIfLoggedIn();
  * This function checks if a user is logged in before the DOM get's rendered. If there is no user logged in, the DOM will not get loaded and the the user will get redirected to the login page.
  */
 function checkIfLoggedIn(){
+	const userObjectForCheck = sessionStorage.getItem("loggedIn");
 	if(userObjectForCheck === null || undefined){
 		window.location.href = "../index.html";
 	}
